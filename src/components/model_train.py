d6c6324e1968b367de0bfec7973494afe6a16124
Shivam Gupta <sg4781778@gmail.com>
2025-06-15 12:03:54 +0530

    Hyperparameter tuning

--- src/components/model_train.py
+++ src/components/model_train.py
@@ -38,17 +38,49 @@ class ModelTrainer:
             models={
                 'RandomForest':RandomForestRegressor(),
                 'DecisionTree':DecisionTreeRegressor(),
-                'Gradient boosting':GradientBoostingRegressor(),
-                'linear Regressor':LinearRegression(),
                 'KNN':KNeighborsRegressor(),
                 'xgb':XGBRegressor(verbosity=0),
                 'catboost':CatBoostRegressor(verbose=0),
                 'adaboost':AdaBoostRegressor()
             }
-            model_report:dict=evaluate_model(X_train,y_train,X_test,y_test,models)
+            params={
+                'randomforest':{
+                    'n_estimators':[100, 200, 300],
+                    'max_depth':[10, 20, 30 ,40],
+                    'min_samples_split':[2, 5, 10],
+                    'min_samples_leaf':[1, 2, 4],
+                },
+                'decisiontree':{
+                    'max_depth':[10, 20, 30],
+                    'min_samples_split':[2, 5, 10],
+                    'min_samples_leaf':[1, 2, 4]
+                },  
+                'knn':{
+                    'n_neighbors':[3, 5, 7, 9],
+                    'weights':['uniform', 'distance'],
+                    'algorithm':['auto', 'ball_tree', 'kd_tree', 'brute']
+                },
+                'xgb':{
+                    'n_estimators':[100, 200, 300],
+                    'learning_rate':[0.01, 0.1, 0.2],
+                    'max_depth':[3, 5, 7]
+                },
+                'catboost':{
+                    'iterations':[100, 200, 300],
+                    'learning_rate':[0.01, 0.1, 0.2],
+                    'depth':[3, 5, 7, 8, 9],
+                    'l2_leaf_reg':[1, 3, 5]
+                },
+                'adaboost':{
+                    'n_estimators':[50, 100, 200],
+                    'learning_rate':[0.01, 0.1, 0.2],
+                    'estimator':[DecisionTreeRegressor(max_depth=3), LinearRegression()]
+                }
+            }
+            model_report:dict=evaluate_model(X_train,y_train,X_test,y_test,models,params)
             best_model_name = max(model_report, key=lambda k: model_report[k]['r2_score'])
             best_model_score = model_report[best_model_name]['r2_score']
-            best_model=models[best_model_name]
+            best_model=model_report[best_model_name]['model']
 
             if best_model_score<0.6:
                 raise CustomException("No best model found with sufficient accuracy", sys)
--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ from src.exception import CustomException
 import pandas as pd
 import numpy as np
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 
 def save_object(file_path, obj):
@@ -20,19 +21,35 @@ def save_object(file_path, obj):
     except CustomException as e:
         raise CustomException(e,sys) from e 
 
-def evaluate_model(X_train, y_train, X_test, y_test, models):
+def evaluate_model(X_train, y_train, X_test, y_test, models, params):
     """Evaluate multiple models and return their performance metrics."""
     
     try:
         report = {}
         for model_name, model in models.items():
-            model.fit(X_train, y_train)
-            y_pred = model.predict(X_test)
+    
+            if param_grid:=params.get(model_name.lower(), {}):
+                grid_search = GridSearchCV(
+                    estimator=model,
+                    param_grid=param_grid,
+                    cv=3,
+                    scoring='r2',
+                    verbose=0,
+                    n_jobs=-1
+                )
+                grid_search.fit(X_train, y_train)
+                best_model = grid_search.best_estimator_
+            else:
+                model.fit(X_train, y_train)
+                best_model = model
+            
+            y_pred = best_model.predict(X_test)
 
             r2 = r2_score(y_test, y_pred)
 
             report[model_name] = {
                 'r2_score': r2,
+                'model': best_model,
             }
         
         return report
